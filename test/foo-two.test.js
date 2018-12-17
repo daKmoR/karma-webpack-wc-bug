a15f27c2022eb82ef5e8b00fab1042ec85fdead4
Thomas Allmer <d4kmor@gmail.com>
2018-12-17 22:04:33 +0100

    chore: use released version + test for dynamic imports

--- test/foo-two.test.js
+++ test/foo-two.test.js
@@ -8,5 +8,8 @@ describe('foo-two', () => {
   it('has foo as two', async () => {
     const foo = document.createElement('foo-two');
     expect(foo.foo).to.equal('two');
+
+    await foo.loadTranslations();
+    expect(foo.foo).to.equal('is great');
   });
 });
